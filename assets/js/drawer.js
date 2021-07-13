1c05a3621fde8c727683dbb6577818697908ce24
DESKTOP-0B9KPCU\Miki Itabashi <ita6.work@gmail.com>
2021-07-13 14:09:16 +0900

    initial upload

---
+++ assets/js/drawer.js
@@ -0,0 +1,12 @@
+"use strict";
+
+jQuery('.drawer-icon').on('click', function (e) {
+  e.preventDefault();
+  jQuery('.drawer-icon').toggleClass('is-active');
+  jQuery('.drawer-content').toggleClass('is-active');
+  jQuery('.drawer__background').toggleClass('is-active');
+  return false;
+});
+jQuery('.drawer-content__item').on('click', function (e) {
+  jQuery('.drawer-icon').trigger('click');
+});
